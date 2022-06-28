0e5fb34fc137d568886b3831c8fce828fa646be7
RedGeorge <94233923+RedGeorge@users.noreply.github.com>
2022-06-28 13:16:12 +0100

    Experimenting with multi-page and wheel component

--- georges-portfolio-site/src/Components/App.js
+++ georges-portfolio-site/src/Components/App.js
@@ -1,37 +1,12 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stars } from '@react-three/drei';
-import React, { Suspense } from 'react';
-import './Styles/App.css';
-import Sun from './Sun';
-import PSPlanet from './PSPlanet';
-import SkillsPlanet from './SkillsPlanet';
-import OtherPlanet from './OtherPlanet';
-import ETPlanet from './ETPlanet';
-import Text3D from './Text';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage/HomePage";
+import Wheel from "./ETPlanet/ETWheel";
 
-function App() {
+
+
+export default function App() {
   return (
-    <Canvas display={'block'} camera={{position: [0, 0, 20], fov: 45}}> {/* Needed to 'draw on' in WebGL/> three.js. Camera sets starting point of view when page loads */}
-      <Suspense fallback={null}>
-        <ambientLight /> 
-        <pointLight position={ [30, 0, 0] } /> 
-        <OrbitControls /> 
-        <Stars /> 
-        <Sun />
-        <PSPlanet/>
-        <SkillsPlanet/>
-        <OtherPlanet/>
-        <ETPlanet/>
-        <Text3D text={''} position={ [0, 0, 0] } size={1}/>
-      </Suspense>
-    </Canvas>
+    <HomePage/>
   );
 }
-
-export default App;
-
-//Comments below cannot be in-line as this messes with the Suspense element, causing it to malfunction with rerendering and breaking the app.
-//{/* <ambientLight /> Shows colour on all planets/sun */}
-// {/* <pointLight position={ [25, 0, 0] } /> {/* Gives effect that sun is emitting light */}
-// <OrbitControls /> {/* Allows user to move camera around space */}
-// <Stars /> Creates starry background (in an orb shape when zoomed out, like the universe) */}
